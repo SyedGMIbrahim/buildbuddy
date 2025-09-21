86e197213aa4c23d6c35e99c7dd531c700374c48
Syed GM Ibrahim <syedgibrahim@gmail.com>
2025-09-21 20:25:49 +0530

    TRPC Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 const Home = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Syed" }));
 
   return (
-    <div>
-      Hello World
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
