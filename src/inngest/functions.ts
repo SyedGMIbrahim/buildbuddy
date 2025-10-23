fd9f19057993f55e9a4329e6b3d0ecdb42518c68
Syed GM Ibrahim <syedgibrahim@gmail.com>
2025-10-23 14:19:49 +0530

    AI Jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+    const codeAgent = createAgent({
+      name: "codeAgent",
+      system: "You are an expert next.js developer. You write readable and maintainable code. You write simple Next.js and React snippets. ",
+      model: gemini({ model: "gemini-2.5-flash" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet : ${event.data.value}` ,
+    );
+    console.log(output);
+
+    return { output };
   },
 );
