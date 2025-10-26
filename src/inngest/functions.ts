dc3de54a76fad5b1197803c24cab66716d049b65
Syed GM Ibrahim <syedgibrahim@gmail.com>
2025-10-26 18:53:18 +0530

    e2b sanboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
+import { Sandbox } from "@e2b/code-interpreter"
 import { gemini, createAgent } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox", async () => {
+      const sandbox = await Sandbox.create("buildbuddy-nextjs-test-1");
+      return sandbox.sandboxId ;
+    })
+
     const codeAgent = createAgent({
       name: "codeAgent",
       system: "You are an expert next.js developer. You write readable and maintainable code. You write simple Next.js and React snippets. ",
@@ -17,6 +24,12 @@ export const helloWorld = inngest.createFunction(
     );
     console.log(output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+
+    return { output, sandboxUrl };
   },
 );
